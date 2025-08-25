412070aa8a8c18398815ffc1b327a18bd11adc70
CarlosJaco <carlos.jacobouski@escola.pr.gov.br>
2025-08-25 15:49:47 -0300

    Update customizacao-e-rotacao.js

--- customizacao-e-rotacao.js
+++ customizacao-e-rotacao.js
@@ -28,7 +28,26 @@ document.getElementById("select-cor").addEventListener("change", (
                  carro-preto-esportivo-lado-direito.png,
                  carro-vermelho-esportivo-lado direito.png,
                  carro-vermelho-esportivo-lado esquerdo.png
-                ) => updateImage());
+                ) => updateImage(                 
+                 carros-preto-normal-lado esquerdo.png,
+                 carros-preto-normal-frente.png,
+                 carros-preto-normal-costas.png,
+                 carro-vermelho-normal-lado esquerdo.png,
+                 carro-vermelho-normal-costas.png,
+                 carro-azul-normal-lado esquerdo.png,
+                 carro-azul-normal-frente.png,
+                 carro-azul-normal-costas.png,
+                 carro-azul-normal-lado-direito.png,
+                 carro-preto-normal-lado-direito.png,
+                 carro-vermelho-normal-frente.png,
+                 carro-vermelho-normal-lado direito.png,
+                 carro-azul-espotivo-lado esquerdo.png,
+                 carros-preto-esportivo-lado esquerdo.png,
+                 carro-azul-espotivo-lado direito.png,
+                 carro-preto-esportivo-lado-direito.png,
+                 carro-vermelho-esportivo-lado direito.png,
+                 carro-vermelho-esportivo-lado esquerdo.png
+));
 document.getElementById("select-rodas").addEventListener("change", (
                  carros-preto-normal-lado esquerdo.png,
                  carros-preto-normal-frente.png,
@@ -48,13 +67,51 @@ document.getElementById("select-rodas").addEventListener("change", (
                  carro-preto-esportivo-lado-direito.png,
                  carro-vermelho-esportivo-lado direito.png,
                  carro-vermelho-esportivo-lado esquerdo.png
-                 ) => updateImage());
+                 ) => updateImage(
+                 carros-preto-normal-lado esquerdo.png,
+                 carros-preto-normal-frente.png,
+                 carros-preto-normal-costas.png,
+                 carro-vermelho-normal-lado esquerdo.png,
+                 carro-vermelho-normal-costas.png,
+                 carro-azul-normal-lado esquerdo.png,
+                 carro-azul-normal-frente.png,
+                 carro-azul-normal-costas.png,
+                 carro-azul-normal-lado-direito.png,
+                 carro-preto-normal-lado-direito.png,
+                 carro-vermelho-normal-frente.png,
+                 carro-vermelho-normal-lado direito.png,
+                 carro-azul-espotivo-lado esquerdo.png,
+                 carros-preto-esportivo-lado esquerdo.png,
+                 carro-azul-espotivo-lado direito.png,
+                 carro-preto-esportivo-lado-direito.png,
+                 carro-vermelho-esportivo-lado direito.png,
+                 carro-vermelho-esportivo-lado esquerdo.png
+));
 
 // Sequência 1: frente → lado direito → costa → lado esquerdo → frente...
 const sequencia1 = ["frente", "lado direito", "costa", "lado esquerdo"];
 let indexSeq1 = 0;
 
-document.getElementById("btn-sequencia1").addEventListener("click", () => {
+document.getElementById("btn-sequencia1").addEventListener("click", (                 
+                 carros-preto-normal-lado esquerdo.png,
+                 carros-preto-normal-frente.png,
+                 carros-preto-normal-costas.png,
+                 carro-vermelho-normal-lado esquerdo.png,
+                 carro-vermelho-normal-costas.png,
+                 carro-azul-normal-lado esquerdo.png,
+                 carro-azul-normal-frente.png,
+                 carro-azul-normal-costas.png,
+                 carro-azul-normal-lado-direito.png,
+                 carro-preto-normal-lado-direito.png,
+                 carro-vermelho-normal-frente.png,
+                 carro-vermelho-normal-lado direito.png,
+                 carro-azul-espotivo-lado esquerdo.png,
+                 carros-preto-esportivo-lado esquerdo.png,
+                 carro-azul-espotivo-lado direito.png,
+                 carro-preto-esportivo-lado-direito.png,
+                 carro-vermelho-esportivo-lado direito.png,
+                 carro-vermelho-esportivo-lado esquerdo.png
+) => {
   indexSeq1 = (indexSeq1 + 1) % sequencia1.length;
   updateImage(sequencia1[indexSeq1]);
 });
@@ -63,7 +120,26 @@ document.getElementById("btn-sequencia1").addEventListener("click", () => {
 const sequencia2 = ["frente", "lado esquerdo", "costa", "lado direito"];
 let indexSeq2 = 0;
 
-document.getElementById("btn-sequencia2").addEventListener("click", () => {
+document.getElementById("btn-sequencia2").addEventListener("click", (                 
+                 carros-preto-normal-lado esquerdo.png,
+                 carros-preto-normal-frente.png,
+                 carros-preto-normal-costas.png,
+                 carro-vermelho-normal-lado esquerdo.png,
+                 carro-vermelho-normal-costas.png,
+                 carro-azul-normal-lado esquerdo.png,
+                 carro-azul-normal-frente.png,
+                 carro-azul-normal-costas.png,
+                 carro-azul-normal-lado-direito.png,
+                 carro-preto-normal-lado-direito.png,
+                 carro-vermelho-normal-frente.png,
+                 carro-vermelho-normal-lado direito.png,
+                 carro-azul-espotivo-lado esquerdo.png,
+                 carros-preto-esportivo-lado esquerdo.png,
+                 carro-azul-espotivo-lado direito.png,
+                 carro-preto-esportivo-lado-direito.png,
+                 carro-vermelho-esportivo-lado direito.png,
+                 carro-vermelho-esportivo-lado esquerdo.png
+) => {
   indexSeq2 = (indexSeq2 + 1) % sequencia2.length;
   updateImage(sequencia2[indexSeq2]);
 });
